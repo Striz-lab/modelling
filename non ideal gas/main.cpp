f37db53525a21249da7b572a24356c3ad2217c28
Daniel <55478231+Striz-lab@users.noreply.github.com>
2021-02-01 13:34:54 +0300

    Add files via upload

--- non ideal gas/main.cpp
+++ non ideal gas/main.cpp
@@ -7,41 +7,36 @@
 
 using namespace std;
 
-    int s = 1000;              //количество частиц
-    double dt = 0.0001;        //время перерасчета
-    int S = 50;                //половина длины ребра куба
-    int N = 100000;            //количество итераций
+int s = 1000;              //количество частиц
+double dt = 0.0001;        //время перерасчета
+int S = 9;                //половина длины ребра куба
+int N = 10000;            //количество итераций
+double E0 = 0;
+double Ep0 = 0;
+double epsilon = 1;
+double sigma = 1;
+double T = 300;
+
+double modulation(double x){
+    if (x > S)
+        x = x - 2 * S;
+    if (x < -S)
+        x = x + 2 * S;
+    return x;
+}
 
 struct pairxv{
     double a, b;
 };
 
-struct pairxv retraction(double x, double v){ //отражение от стенок
-    if (x > S){
-        v = - v;
-        x = S - 1;
-    }
-    else{
-        if (x < - S){
-            v = - v;
-            x = - S + 1;
-        }
-    }
-    
-    if (abs(v) > pow(10, 2))
-        v = (rand() % 100) - 50;
-    struct pairxv tmp = {x, v};
-    return tmp;
-}
-
-struct pairxv teleportation(double x, double v){ //телепортация частичек
-    if (x > S)
-        x = - S;
-    if (x < -S)
-        x = S;
-    
-    if (abs(v) > pow(10, 2))
-        v = (rand() % 100) - 50;
+struct pairxv teleportation(double x, double v, double E){ //телепортация частичек
+    x = modulation(x);
+    //if (v < 10)
+        //v = v*1.1;
+    //if (abs(E) < 0.7*E0)
+        //v = 1.1 * v;
+    //if (abs(E) > 1.3*E0)
+        //v = v / 1.1;
     struct pairxv tmp = {x, v};
     return tmp;
 }
@@ -57,10 +52,12 @@ int main(){
     fout3.open("Maxwell.csv");
     fout4.open("Diffuziya.csv");
 
-    double x[s],  y[s],  z[s],            //координаты
-           vx[s], vy[s], vz[s],           //скорости
-           ax[s], ay[s], az[s],           //ускорения
-           xnach[s], ynach[s], znach[s];  //начальные координаты (для диффузии)
+    double x[s],     y[s],     z[s],            //координаты
+           xpr[s],   ypr[s],   zpr[s],          //координаты в предыдущий момент времени для уменьшения ошибки
+           X, Y, Z, //пока ничего лучше не придумал
+           vx[s],    vy[s],    vz[s],           //скорости
+           ax[s],    ay[s],    az[s],           //ускорения
+           xnach[s], ynach[s], znach[s];        //начальные координаты (для диффузии)
 
     double  Ekin = 0, Ep = 0, dif = 0, a,
             r, rx, ry, rz;
@@ -69,31 +66,81 @@ int main(){
     array<int, 100> nV;
     
     for (int i = 0; i < s; i++){
-        x[i] = (i % 10) * 10 - 45;
-        y[i] = ((i / 10) % 10) * 10 - 45;
-        z[i] = ((i / 100) % 10) * 10 - 45;
-        vx[i] = (rand() % 10000) - 5000;
-        vy[i] = (rand() % 10000) - 5000;
-        vz[i] = (rand() % 10000) - 5000;
+        x[i] = ((double)rand() / RAND_MAX)*2*S - S;
+        y[i] = ((double)rand() / RAND_MAX)*2*S - S;
+        z[i] = ((double)rand() / RAND_MAX)*2*S - S;
+        for (int j = 0; j < i; j++){
+            rx = modulation(x[i] - x[j]);
+            ry = modulation(y[i] - y[j]);
+            rz = modulation(z[i] - z[j]);
+
+            r = rx * rx + ry * ry + rz * rz;
+            
+            if (r < 0.3){
+                x[i] = ((double)rand() / RAND_MAX)*2*S - S;
+                y[i] = ((double)rand() / RAND_MAX)*2*S - S;
+                z[i] = ((double)rand() / RAND_MAX)*2*S - S;
+            }
+        }
+        
+//температура
+        
+        
+         
+         double V = pow(T, 0.5);
+         double ranx = (rand() % 1000) - 500;
+         double rany = (rand() % 1000) - 500;
+         double ranz = (rand() % 1000) - 500;
+         double ran = pow(ranx * ranx + rany * rany + ranz * ranz, 0.5);
+         if (ran == 0){
+             ranx = 1;
+             rany = 1;
+             ranz = 1;
+             ran = pow(ranx * ranx + rany * rany + ranz * ranz, 0.5);
+         }
+         
+        xpr[i] = x[i] - V * dt * ranx / ran;
+        ypr[i] = y[i] - V * dt * rany / ran;
+        zpr[i] = z[i] - V * dt * ranz / ran;
+        /*vx[i] = (rand() % 10) - 5;
+        vy[i] = (rand() % 10) - 5;
+        vz[i] = (rand() % 10) - 5;*/
         ax[i] = 0;
         ay[i] = 0;
         az[i] = 0;
         xnach[i] = x[i];
         ynach[i] = y[i];
         znach[i] = z[i];
+        /*xpr[i] = x[i] - vx[i] * dt;
+        ypr[i] = y[i] - vy[i] * dt;
+        zpr[i] = z[i] - vz[i] * dt;*/
+        //E0 += (vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i])/2;
+        
+        for (int j = 0; j < i; j++){
+            rx = modulation(x[i] - x[j]);
+            ry = modulation(y[i] - y[j]);
+            rz = modulation(z[i] - z[j]);
+
+            r = rx * rx + ry * ry + rz * rz;
+        
+        if ((r != 0) and (r <= 50))
+            Ep0 = Ep0 + 24*epsilon*(pow((r/sigma), -6) - pow((r/sigma), -3));
+    
+        }
+        E0 = Ep0 + E0;
     }
     
     for (int ch = 0; ch <= N; ch++){
         for (int i = 0; i < s; i++){
             for (int j = 0; j < i; j++){
-                    rx = x[i] - x[j];
-                    ry = y[i] - y[j];
-                    rz = z[i] - z[j];
+                    rx = modulation(x[i] - x[j]);
+                    ry = modulation(y[i] - y[j]);
+                    rz = modulation(z[i] - z[j]);
 
                     r = rx * rx + ry * ry + rz * rz;
                 
-                if (r != 0){
-                    a = 24*(-2*pow(r, -7) + pow(r, -4));
+                if (r <= 50){
+                    a = 24*epsilon*(-2*pow((r/sigma), -7) + pow((r/sigma), -4))/(sigma*sigma);
                     
                     ax[i] = ax[i] - a * rx;
                     ay[i] = ay[i] - a * ry;
@@ -103,7 +150,7 @@ int main(){
                     ay[j] = ay[j] + a * ry;
                     az[j] = az[j] + a * rz;
                     
-                    Ep = Ep + 24*(pow(r, -6) - pow(r, -3));
+                    Ep = Ep + 24*epsilon*(pow((r/sigma), -6) - pow((r/sigma), -3));
                 }
             }
         }
@@ -115,35 +162,36 @@ int main(){
         }
         
         for (int i = 0; i < s; i++){
+            
+            X = x[i];
+            Y = y[i];
+            Z = z[i];
+            
             vx[i] = vx[i] + ax[i] * dt;
             vy[i] = vy[i] + ay[i] * dt;
             vz[i] = vz[i] + az[i] * dt;
             
-            x[i] = x[i] + vx[i] * dt + ax[i] * dt * dt / 2;
-            y[i] = y[i] + vy[i] * dt + ay[i] * dt * dt / 2;
-            z[i] = z[i] + vz[i] * dt + az[i] * dt * dt / 2;
+            //x[i] = x[i] + vx[i] * dt + ax[i] * dt * dt / 2;
+            //y[i] = y[i] + vy[i] * dt + ay[i] * dt * dt / 2;
+            //z[i] = z[i] + vz[i] * dt + az[i] * dt * dt / 2;
             
-            /*struct pairxv pairx = retraction(x[i], vx[i]);
+            x[i] = 2 * x[i] - xpr[i] + ax[i] * dt * dt;
+            y[i] = 2 * y[i] - ypr[i] + ay[i] * dt * dt;
+            z[i] = 2 * z[i] - zpr[i] + az[i] * dt * dt;
+            
+            xpr[i] = X;
+            ypr[i] = Y;
+            zpr[i] = Z;
+            
+            struct pairxv pairx = teleportation(x[i], vx[i], Ekin + Ep);
             x[i] = pairx.a;
             vx[i] = pairx.b;
             
-            struct pairxv pairy = retraction(y[i], vy[i]);
+            struct pairxv pairy = teleportation(y[i], vy[i], Ekin + Ep);
             y[i] = pairy.a;
             vy[i] = pairy.b;
             
-            struct pairxv pairz = retraction(z[i], vz[i]);
-            z[i] = pairz.a;
-            vz[i] = pairz.b;*/
-            
-            struct pairxv pairx = teleportation(x[i], vx[i]);
-            x[i] = pairx.a;
-            vx[i] = pairx.b;
-            
-            struct pairxv pairy = teleportation(y[i], vy[i]);
-            y[i] = pairy.a;
-            vy[i] = pairy.b;
-            
-            struct pairxv pairz = teleportation(z[i], vz[i]);
+            struct pairxv pairz = teleportation(z[i], vz[i], Ekin + Ep);
             z[i] = pairz.a;
             vz[i] = pairz.b;
             
@@ -151,9 +199,11 @@ int main(){
             ay[i] = 0;
             az[i] = 0;
             
-            Ekin = Ekin + (vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i])/2;
-            dif = dif + (x[i] - xnach[i]) * (x[i] - xnach[i]) + (y[i] - ynach[i]) * (y[i] - ynach[i]) + (z[i] - znach[i]) * (z[i] - znach[i]);
-            Ekin = Ekin + (x[i] - xnach[i]) * (x[i] - xnach[i]) + (y[i] - ynach[i]) * (y[i] - ynach[i]) + (z[i] - znach[i]) * (z[i] - znach[i])/(dt * dt);
+            //Ekin = Ekin + (vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i])/2;
+            //dif = dif + (modulation(x[i] - xnach[i])) * modulation((x[i] - xnach[i])) + modulation((y[i] - ynach[i])) * modulation((y[i] - ynach[i])) + modulation((z[i] - znach[i])) * modulation((z[i] - znach[i]));
+            dif = dif + ((x[i] - xnach[i])) * ((x[i] - xnach[i])) + ((y[i] - ynach[i])) * ((y[i] - ynach[i])) + ((z[i] - znach[i])) * ((z[i] - znach[i]));
+            Ekin = Ekin + modulation((x[i] - xpr[i])) * modulation((x[i] - xpr[i])) + modulation((y[i] - ypr[i])) * modulation((y[i] - ypr[i])) + modulation((z[i] - zpr[i])) * modulation((z[i] - zpr[i]))/(2 * dt * dt);
+            T = Ekin/s;
         }
     
         fout2 << ch << "," << fixed << Ep << "," << fixed << Ekin << "," << fixed << Ekin + Ep << "\n";
@@ -162,10 +212,11 @@ int main(){
         
         Ekin = 0;
         Ep = 0;
+        dif = 0;
     }
     
     for (int i = 0; i < s; i++){ //Максвелл начало (для последнего шага)
-        V[i] = pow((vx[i] * vx[i] + vy[i] * vy[i] + vz[i] * vz[i]), 0.5);
+        V[i] = pow((modulation((x[i] - xpr[i])) * modulation((x[i] - xpr[i])) + modulation((y[i] - ypr[i])) * modulation((y[i] - ypr[i])) + modulation((z[i] - zpr[i])) * modulation((z[i] - zpr[i]))/( dt * dt)), 0.5);
         nV[i] = 0;
     }
     sort(V.begin(), V.end());
@@ -173,12 +224,12 @@ int main(){
     double B = 0;
     for (int j = 0; j < s; j++){
         for (int i = 0; i < s; i++)
-            if ((V[i] <= B + 10 * A) and (V[i] >= B - 10 * A))
+            if ((V[i] <= B + 50 * A) and (V[i] >= B - 50 * A))
                 nV[j] ++;
         B += A;
     }
     for (int i = 0; i < s; i++)
-        if ((nV[i] != 0) and (C < 0.7*V[999]) and (nV[i] < 200))
+        //if (nV[i] < 450)
             fout3 << i * A << "," << nV[i] << "\n"; //Максвелл конец
 
     fout1.close();
--- non ideal gas/plott.py
+++ non ideal gas/plott.py
@@ -1,6 +1,8 @@
 import csv
+import math
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
@@ -12,7 +14,7 @@ def Diffusion():
         rdr = [list(read) for read in reader][3::]
 
     fig, ax = plt.subplots()
-    ax.plot([float(r[0]) for r in rdr], [float(r[1])**0.5 for r in rdr], 'r.-')
+    ax.plot([float(r[0]) for r in rdr], [float(r[1]) for r in rdr], 'r.-')
     ax.grid(linestyle='--', linewidth=0.5)
     
     ax.xaxis.set_minor_locator(AutoMinorLocator())
@@ -26,16 +28,25 @@ def Diffusion():
     fig.savefig('Diffusion.png')
     
 def Maxwell():
+    mu = 100  # mean of distribution
+    sigma = 15  # standard deviation of distribution
     with open('Maxwell.csv'.format(1), 'r') as f:
         reader = csv.reader(f)
         rdr = [list(read) for read in reader][3::]
 
-    fig, ax = plt.subplots()
-    ax.plot([float(r[0]) for r in rdr], [float(r[1]) for r in rdr], '.')
-    ax.grid(linestyle='--', linewidth=0.5)
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4), sharey=True)
+    ax.bar([float(r[0]) for r in rdr], [float(r[1]) for r in rdr])
+    #ax.plot( [(float(r[0])**2) for r in rdr],[np.log1p((float(r[1])/(float(r[0])**2))) for r in rdr], '.')
+    
+    #y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+    #ax.plot(bins, y, '--')
+    #ax[1].grid(linestyle='--', linewidth=0.5)
 
     ax.xaxis.set_minor_locator(AutoMinorLocator())
     ax.yaxis.set_minor_locator(AutoMinorLocator())
+    
+    #ax.xaxis.set_minor_locator(AutoMinorLocator())
+    #ax.yaxis.set_minor_locator(AutoMinorLocator())
 
     ax.set_ylabel(r'$N$')
     ax.set_xlabel(r'$V$')
@@ -52,7 +63,7 @@ def Energy():
 
     fig, ax = plt.subplots()
     ax.plot([float(r[0]) for r in rdr], [float(r[1]) for r in rdr], '.', color='tab:blue')
-    ax.plot([float(r[0]) for r in rdr], [0.5*float(r[2]) for r in rdr], '.', color='tab:red')
+    ax.plot([float(r[0]) for r in rdr], [float(r[2]) for r in rdr], '.', color='tab:red')
     ax.plot([float(r[0]) for r in rdr], [float(r[3]) for r in rdr], '.', color='tab:purple')
     ax.grid(linestyle='--', linewidth=0.5)
 
@@ -65,7 +76,7 @@ def Energy():
         
     plt.show()
     fig.savefig('Energy.png')
-    
+
 Diffusion()
 Maxwell()
 Energy()
