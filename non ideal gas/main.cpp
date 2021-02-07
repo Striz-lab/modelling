f6259eed3951fed92ce14f805c1581eefec60b16
Daniel <55478231+Striz-lab@users.noreply.github.com>
2021-02-07 15:20:08 +0300

    I think its final version

--- non ideal gas/main.cpp
+++ non ideal gas/main.cpp
@@ -3,51 +3,45 @@
 #include <cmath>
 #include <array>
 #include <ctime>
-#include <string>
 
 using namespace std;
 
-int s = 1000;             //количество частиц
+int s = 1000;               //количество частиц
 double dt = 0.0001;         //время перерасчета
-double S = 8;                 //половина длины ребра куба
-int N = 500000;            //количество итераций
-double epsilon = 1;
-double sigma = 1;
-double T = 3;
-double Rmin = 0.7;  //минимально допустимое расстояние для генерации (квадрат, линейно -- 0.83)
+double S = 8;               //половина длины ребра куба
+int N = 100000;             //количество итераций
+double eps = 1, sigma = 1;  //параметры потенциала (лучше не менять)
+double T = 3;               //аналог температуры
+double Rmin = 0.7;          //минимально допустимое расстояние для генерации (квадрат, линейно -- 0.83)
 
-double modulation(double x){
-    if (x > S){
+double modulation(double x){//функция для периодических граничных условий
+    if (x > S)              //и для перерасчета расстояний между частицами
         x = x - 2 * S;
-    }
-    if (x < -S){
+    if (x < -S)
         x = x + 2 * S;
-    }
     return x;
 }
 
-
-double correction(double x, double y){
-    if (abs(x) > S)
+double correction(double x, double y){ //аналогичная функция для коррекции диффузии
+    if (x > S)
+        y = y + 2 * S;
+    if (x < -S)
         y = y - 2 * S;
     return y;
 }
 
-double rmin(double *x, double *y, double *z, int NOW){
-    double rmin = 100;
-        for (int j = 0; j < NOW; j++){
+double rmin(double *x, double *y, double *z, int NOW){ //нахождение минимального
+    double rmin = 100;                                 //расстояния между
+        for (int j = 0; j < NOW; j++){                 //уже созданными частицами
             double rx = modulation(x[NOW] - x[j]);
             double ry = modulation(y[NOW] - y[j]);
             double rz = modulation(z[NOW] - z[j]);
-
             double r = rx * rx + ry * ry + rz * rz;
-            
             if (r < rmin)
                 rmin = r;
         }
     return rmin;
 }
-
 
 int main(){
     time_t start, end;
@@ -56,24 +50,24 @@ int main(){
     ofstream fout1, fout2, fout3, fout4, fout5;
     fout1.open("Coor.xyz");
     fout2.open("Energy.csv");
-    fout3.open("Maxwell.csv");
-    fout4.open("Diffuziya.csv");
+    fout3.open("Velocities.csv");
+    fout4.open("Diffusion.csv");
     fout5.open("Temperature.csv");
 
     double x[s],     y[s],     z[s],            //координаты
-           xpr[s],   ypr[s],   zpr[s],          //координаты в предыдущий момент времени для уменьшения ошибки
-           X, Y, Z, //пока ничего лучше не придумал
+           xpr[s],   ypr[s],   zpr[s],          //координаты в предыдущий момент
+                                                //времени для уменьшения ошибки
+           X,        Y,        Z,               //пока ничего лучше не придумал
            vx[s],    vy[s],    vz[s],           //скорости
            ax[s],    ay[s],    az[s],           //ускорения
-           xnach[s], ynach[s], znach[s];        //начальные координаты (для диффузии)
+           xnach[s], ynach[s], znach[s],        //начальные координаты (для
+           Ekin = 0, Ep = 0, dif = 0, a,        //диффузии)
+           r = 0, rx, ry, rz;
 
-    double  Ekin = 0, Ep = 0, dif = 0, a,
-            r = 0, rx, ry, rz;
-    
     array<double, 1000> V;
     array<int, 100> nV;
 
-    for (int i = 0; i < s; i++){
+    for (int i = 0; i < s; i++){                //случайная генерация частиц
         x[i] = ((double)rand() / RAND_MAX)*2*(S - Rmin) - (S - Rmin);
         y[i] = ((double)rand() / RAND_MAX)*2*(S - Rmin) - (S - Rmin);
         z[i] = ((double)rand() / RAND_MAX)*2*(S - Rmin) - (S - Rmin);
@@ -83,52 +77,52 @@ int main(){
             y[i] = ((double)rand() / RAND_MAX)*2*(S - Rmin) - (S - Rmin);
             z[i] = ((double)rand() / RAND_MAX)*2*(S - Rmin) - (S - Rmin);
             
-            rm = rmin(x,y,z,i);
-            //cout << x[i] << " " << y[i] << " " << z[i] << " " << rm << endl;
+            rm = rmin(x,y,z,i);                 //проверка, чтобы не улетело далеко
         }
-        cout << rmin(x,y,z,i) << " " << i << endl;
+        //cout << rmin(x,y,z,i) << " " << i << endl;
     }
-        
-//температура
-    for (int i = 0; i<s; i++){
-         double V = pow(3*T, 0.5);
-         double ranx = ((double)rand() / RAND_MAX)*2*S - S;
-         double rany = ((double)rand() / RAND_MAX)*2*S - S;
-         double ranz = ((double)rand() / RAND_MAX)*2*S - S;
-         double ran = pow(ranx * ranx + rany * rany + ranz * ranz, 0.5);
+    for (int i = 0; i < s; i++){
+        double V = pow(3*T, 0.5);              //инициализация направлений
+        double ranx = ((double)rand() / RAND_MAX)*2*S - S;
+        double rany = ((double)rand() / RAND_MAX)*2*S - S;
+        double ranz = ((double)rand() / RAND_MAX)*2*S - S;
+        double ran = pow(ranx * ranx + rany * rany + ranz * ranz, 0.5);
          
-        xpr[i] = x[i] - V * dt * ranx / ran;
-        ypr[i] = y[i] - V * dt * rany / ran;
-        zpr[i] = z[i] - V * dt * ranz / ran;
+        xpr[i] = x[i] - V * dt * ranx / ran;    //координаты частиц системы
+        ypr[i] = y[i] - V * dt * rany / ran;    //на предыдущем шаге для
+        zpr[i] = z[i] - V * dt * ranz / ran;    //более точного рассчета
+        
+        vx[i] = V * ranx / ran;                 //инициализация скоростей
+        vy[i] = V * rany / ran;
+        vz[i] = V * ranz / ran;
 
-        ax[i] = 0;
+        ax[i] = 0;                              //инициализация ускорений
         ay[i] = 0;
         az[i] = 0;
-        xnach[i] = x[i];
-        ynach[i] = y[i];
+        
+        xnach[i] = x[i];                        //инициализация начальных положений
+        ynach[i] = y[i];                        //частичек
         znach[i] = z[i];
     }
     
-    for (int ch = 0; ch <= N; ch++){
+    for (int ch = 0; ch <= N; ch++){            //рассчет ускорений
         for (int i = 0; i < s; i++){
             for (int j = 0; j < i; j++){
                 rx = modulation(x[i] - x[j]);
                 ry = modulation(y[i] - y[j]);
                 rz = modulation(z[i] - z[j]);
-
                 r = rx * rx + ry * ry + rz * rz;
 
-                a = 24*epsilon*(-2*pow((sigma/r), 7) + pow((sigma/r), 4))/(sigma*sigma);
-                    
+                a = 24*eps*(-2*pow((sigma/r), 7) + pow((sigma/r), 4))/(sigma*sigma);
+                Ep = Ep + 4*eps*(pow((sigma/r), 6) - pow((sigma/r), 3));
+                
                 ax[i] = ax[i] - a * rx;
                 ay[i] = ay[i] - a * ry;
                 az[i] = az[i] - a * rz;
-
+                
                 ax[j] = ax[j] + a * rx;
                 ay[j] = ay[j] + a * ry;
                 az[j] = az[j] + a * rz;
-                    
-                Ep = Ep + 4*epsilon*(pow((sigma/r), 6) - pow((sigma/r), 3));
             }
         }
         
@@ -138,40 +132,33 @@ int main(){
                 fout1 << i << "\t" << x[i] << "\t" << y[i] << "\t" << z[i] << "\n";
         }
         
-        for (int i = 0; i < s; i++){
+        for (int i = 0; i < s; i++){        //обновление координат и скоростей
             
             X = x[i];
             Y = y[i];
             Z = z[i];
             
-            vx[i] = vx[i] + ax[i] * dt;
-            vy[i] = vy[i] + ay[i] * dt;
-            vz[i] = vz[i] + az[i] * dt;
-            
-            x[i] = 2 * x[i] - xpr[i] + ax[i] * dt * dt;
-            y[i] = 2 * y[i] - ypr[i] + ay[i] * dt * dt;
-            z[i] = 2 * z[i] - zpr[i] + az[i] * dt * dt;
+            x[i] = modulation(2 * x[i] - xpr[i] + ax[i] * dt * dt);//более точный
+            y[i] = modulation(2 * y[i] - ypr[i] + ay[i] * dt * dt);//рассчет вместе
+            z[i] = modulation(2 * z[i] - zpr[i] + az[i] * dt * dt);//с коррекцией
             
             xpr[i] = X;
             ypr[i] = Y;
             zpr[i] = Z;
             
-            xnach[i] = correction(x[i] - xpr[i], xnach[i]);
+            vx[i] = vx[i] + ax[i] * dt;
+            vy[i] = vy[i] + ay[i] * dt;
+            vz[i] = vz[i] + az[i] * dt;
+            
+            xnach[i] = correction(x[i] - xpr[i], xnach[i]); //коррекция
             ynach[i] = correction(y[i] - ypr[i], ynach[i]);
             znach[i] = correction(z[i] - zpr[i], znach[i]);
             
-            x[i] = modulation(x[i]);
-            y[i] = modulation(y[i]);
-            z[i] = modulation(z[i]);
-            
-            
-            
-            ax[i] = 0;
+            ax[i] = 0;                                      //обновление
             ay[i] = 0;
             az[i] = 0;
             
             dif = dif + ((x[i] - xnach[i])) * ((x[i] - xnach[i])) + ((y[i] - ynach[i])) * ((y[i] - ynach[i])) + ((z[i] - znach[i])) * ((z[i] - znach[i]));
-            
             Ekin = Ekin + (modulation((x[i] - xpr[i])) * modulation((x[i] - xpr[i])) + modulation((y[i] - ypr[i])) * modulation((y[i] - ypr[i])) + modulation((z[i] - zpr[i])) * modulation((z[i] - zpr[i])))/(2 * dt * dt);
         }
         if (ch % 10 == 0){
@@ -179,14 +166,13 @@ int main(){
         fout4 << ch << "," << fixed << dif / s << "\n";
         fout5 << ch << "," << fixed << 2*Ekin / (3*s) << "\n";
         }
- 
         Ekin = 0;
         Ep = 0;
         dif = 0;
     }
     
     for (int i = 0; i < s; i++){ //Максвелл начало (для последнего шага)
-        V[i] = pow((modulation((x[i] - xpr[i])) * modulation((x[i] - xpr[i])) + modulation((y[i] - ypr[i])) * modulation((y[i] - ypr[i])) + modulation((z[i] - zpr[i])) * modulation((z[i] - zpr[i]))/( dt * dt)), 0.5);
+        V[i] = pow(vx[i]*vx[i] + vy[i]*vy[i] +vz[i]*vz[i], 0.5);
         nV[i] = 0;
     }
     sort(V.begin(), V.end());
@@ -194,13 +180,12 @@ int main(){
     double B = 0;
     for (int j = 0; j < s; j++){
         for (int i = 0; i < s; i++)
-            if ((V[i] <= B +  20*A) and (V[i] >= B - 20*A))
+            if ((V[i] != 0) and (V[i] <= B + 60*A) and (V[i] >= B - 60*A))
                 nV[j] ++;
         B += A;
     }
     for (int i = 0; i < s; i++)
-        if (nV[i] < 400)
-            fout3 << i * A << "," << nV[i] << "\n"; //Максвелл конец
+        fout3 << i * A << "," << nV[i] << "\n"; //Максвелл конец
 
     fout1.close();
     fout2.close();
--- non ideal gas/plott.py
+++ non ideal gas/plott.py
@@ -5,101 +5,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
+'''
+Рисует картинку
+file -- название файла без расширения
+n -- количество кривых на рисунке
+Ox -- подпись к оси Ох
+Oy -- подпись к оси Оу
+Caption -- подпись к графику
+name -- название сохраненной картинки с расширением
+a -- костыль, позволяющий построить график в нелинейных осях
+'''
 
-
-
-def Diffusion():
-    with open('Diffuziya.csv'.format(1), 'r') as f:
+def picture(file, n, Ox, Oy, Caption, name, a = 0):
+    with open(file + '.csv'.format(1), 'r') as f:
         reader = csv.reader(f)
         rdr = [list(read) for read in reader][3::]
-
+        
     fig, ax = plt.subplots()
-    ax.plot([float(r[0]) for r in rdr], [float(r[1]) for r in rdr], 'r.-')
+    if a == 1:
+        ax.plot( [np.log1p((float(r[1]))/(((float(r[0])**2)))) for r in rdr],[(float(r[0])**2) for r in rdr], '.')
+    else:
+        for i in range(n):
+            ax.plot([float(r[0]) for r in rdr], [float(r[i + 1]) for r in rdr], 'r.-', color='#'+ str(int((i - 1)*(i - 2)*4.5))+ str(0) + str(i*(2 - i)*8) + str(0) + str(i*(i - 1)*4) + str(0))
+            
     ax.grid(linestyle='--', linewidth=0.5)
     
     ax.xaxis.set_minor_locator(AutoMinorLocator())
     ax.yaxis.set_minor_locator(AutoMinorLocator())
-    
-    ax.set_ylabel(r'$<\bigtriangleup r>$')
-    ax.set_xlabel(r'$t$')
-    ax.set_title('Диффузия')
-        
+    ax.set_ylabel(Oy)
+    ax.set_xlabel(Ox)
+    ax.set_title(Caption)
+            
     plt.show()
-    fig.savefig('Diffusion.png')
-    
-def Maxwell():
-    mu = 100  # mean of distribution
-    sigma = 15  # standard deviation of distribution
-    with open('Maxwell.csv'.format(1), 'r') as f:
-        reader = csv.reader(f)
-        rdr = [list(read) for read in reader][3::]
+    fig.savefig(name)
 
-    fig, ax = plt.subplots(1, 1, figsize=(8, 4), sharey=True)
-    ax.bar([float(r[0]) for r in rdr], [float(r[1]) for r in rdr])
-    #ax.plot( [np.log1p((float(r[1])/(float(r[0])**2))) for r in rdr],[(float(r[0])**2) for r in rdr], '.')
-    
-    #y = ((1 / (np.sqrt(2 * np.pi) * sigma)) * np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
-    #ax.plot(bins, y, '--')
-    #ax[1].grid(linestyle='--', linewidth=0.5)
 
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-    
-    #ax.xaxis.set_minor_locator(AutoMinorLocator())
-    #ax.yaxis.set_minor_locator(AutoMinorLocator())
-
-    ax.set_ylabel(r'$N$')
-    ax.set_xlabel(r'$V$')
-    ax.set_title('Распределение Максвелла по модулям скоростей')
-        
-    plt.show()
-    fig.savefig('Maxwell.png')
-    
-    
-def Energy():
-    with open('Energy.csv'.format(1), 'r') as f:
-        reader = csv.reader(f)
-        rdr = [list(read) for read in reader][3::]
-
-    fig, ax = plt.subplots()
-    ax.plot([float(r[0]) for r in rdr], [float(r[1]) for r in rdr], '.', color='tab:blue')
-    ax.plot([float(r[0]) for r in rdr], [float(r[2]) for r in rdr], '.', color='tab:red')
-    
-    ax.plot([float(r[0]) for r in rdr], [float(r[3]) for r in rdr], '.', color='tab:purple')
-    ax.grid(linestyle='--', linewidth=0.5)
-
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-
-    ax.set_ylabel(r'$N$')
-    ax.set_xlabel(r'$V$')
-    ax.set_title('Энергии')
-        
-    plt.show()
-    fig.savefig('Energy.png')
-    
-    
-def Temperature():
-    with open('Temperature.csv'.format(1), 'r') as f:
-        reader = csv.reader(f)
-        rdr = [list(read) for read in reader][3::]
-
-    fig, ax = plt.subplots()
-    ax.plot([float(r[0]) for r in rdr], [float(r[1]) for r in rdr], 'r.-')
-    ax.grid(linestyle='--', linewidth=0.5)
-    
-    ax.xaxis.set_minor_locator(AutoMinorLocator())
-    ax.yaxis.set_minor_locator(AutoMinorLocator())
-    
-    ax.set_ylabel(r'$<\bigtriangleup r>$')
-    ax.set_xlabel(r'$t$')
-    ax.set_title('температура от времени')
-        
-    plt.show()
-    fig.savefig('Temperature.png')
-
-Diffusion()
-Maxwell()
-Energy()
-Temperature()
+picture('Diffusion', 1, r'$t$', r'$<\Delta r^2>$', 'Диффузия', 'Diffusion.png')
+picture('Velocities', 1, r'$V$', r'$N$', 'Распределение Максвелла по модулям скоростей', 'Maxwell.png')
+picture('Velocities', 1, r'$V^2$', r'$\ln{\frac{N}{V^2}}$', 'Линеаризованное распределение Максвелла по модулям скоростей', 'Maxwell_lin.png', 1)
+picture('Energy', 3, r'$t$', r'$E$', 'Зависимость энергии от времени', 'Energy.png')
+picture('Temperature', 1, r'$t$', r'$T$', 'Зависимость температуры от времени', 'Temperature.png')
 
